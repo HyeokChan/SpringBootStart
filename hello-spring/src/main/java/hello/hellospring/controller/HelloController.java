ee7e79eff7d6805eb4e3f2a335358a6841ac3922
HyeokChan <qpjiq123@gmail.com>
2021-08-13 00:06:07 +0900

    spring mvc 패턴 학습

---
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,26 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    /*get방식으로 hello 넘어왔을 때 실행*/
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello");
+        /* resources/templates/hello.html 실행
+        * 컨트롤러에서 리턴 값으로 문자를 반환하면 뷰 리졸버(viewResolver)가 화면을 찾아서 처리한다.*/
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+
+}
