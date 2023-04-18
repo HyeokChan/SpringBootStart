8183a1b806aaa011b44e6e388339e3ed6b2ecaab
qpjiq123@gmail.com <qpjiq123@gmail.com>
2023-04-19 00:07:36 +0900

    db접근기술 복습

--- hello-spring/src/main/java/hello/hellospring/repository/JpaMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/JpaMemberRepository.java
@@ -1,6 +1,8 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
--- hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface SpringDataJapMemberRepository extends JpaRepository<Member, Long>/*, MemberRepository*/ {
+// JpaRepository 를 상속받으면서 자동으로 bean에 등록한다.
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
 
     // 인터페이스 메소드 이름으로도 쿼리를 짤 수 있다.
     //JPQL select m from Member m where m.name = ?
-//    @Override
-//    Optional<Member> findByName(String name);
+    @Override
+    Optional<Member> findByName(String name);
 }
